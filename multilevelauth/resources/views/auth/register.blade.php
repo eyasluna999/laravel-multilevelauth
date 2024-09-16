0b1151af49f82f314be340f0c554b36e8c44bec8
eyasluna999 <dalidagab01423@gmail.com>
2024-09-16 08:31:33 +0800

    09/12/24
    - added multilevel authentication
    - added user's registration credentials

---
+++ multilevelauth/resources/views/auth/register.blade.php
@@ -0,0 +1,89 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+        <!-- Contact Information -->
+        <div class="mt-4">
+            <x-input-label for="contactNum" :value="__('Contact Number')" />
+            <x-text-input id="contactNum" class="block mt-1 w-full" type="number" name="contactNum"  pattern="\d{1,11}" :value="old('contactNum')" oninput="limitInput(this)" required autocomplete="contactNum" />
+            <x-input-error :messages="$errors->get('contact number')" class="mt-2" />
+        </div>
+
+        <!-- Street -->
+        <div class="mt-4">
+            <x-input-label for="street" :value="__('Street')" />
+            <x-text-input id="street" class="block mt-1 w-full" type="text" name="street" :value="old('street')" required autocomplete="street" />
+            <x-input-error :messages="$errors->get('street')" class="mt-2" />
+        </div>
+
+        <!-- City -->
+        <div class="mt-4">
+            <x-input-label for="city" :value="__('City')" />
+            <x-text-input id="city" class="block mt-1 w-full" type="text" name="city" :value="old('city')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('city')" class="mt-2" />
+        </div>
+
+        <!-- State -->
+        <div class="mt-4">
+            <x-input-label for="state" :value="__('State')" />
+            <x-text-input id="state" class="block mt-1 w-full" type="text" name="state" :value="old('state')" required autocomplete="state" />
+            <x-input-error :messages="$errors->get('state')" class="mt-2" />
+        </div>
+
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
+
+<script>
+    function limitInput(input){
+        input.value = input.value.replace(/\D/g, '');
+        if (input.value.length > 11){
+            input.value = input.value.slice(0,11);
+        }
+    }
+</script>
